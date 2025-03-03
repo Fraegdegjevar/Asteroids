377fed1f8310b6d9264e90ab8c4728ac346a64c3
Fraegdegjevar <willhcode@gmail.com>
2025-03-03 22:10:32 +0000

    Create circleshape and player classes and methods, add pygame groups as containers for Player class

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from constants import *
+from player import *
 import pygame
 
 def main():
     pygame.init()
     GameClock = pygame.time.Clock()
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # type: ignore
     
+    #Creating groups for updatable and drawable objects
+    # These can be added as containers to each class and will ensure
+    #all instances are in the group
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # type: ignore
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill(color = (1,1,1))
+        
+        updatable.update(dt)
+        
+        for object in drawable:
+            object.draw(screen)
+        
         pygame.display.flip()
         
         #limit frame rate to 60 fps and store delta time in dt
